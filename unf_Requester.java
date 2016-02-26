3e86431c51ac9089c717eb05af1940b805b4cc08
James Spinella <jspinella@jatsby.com>
2016-02-26 13:39:12 -0500

    Initial Commit
    Robert’s stuff

---
+++ unf_Requester.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.lang.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Requester {
+
+  //  InetAddress host = InetAddress.getLocalHost();
+    //  Socket socket = new Socket(host.getHostName(), mySocket);
+    public static void main(String[] args) {
+        System.out.println("Starting client main...\n");
+        int menuItem = 1;
+        Socket socket;
+        int mySocket = 20904; // 31337;
+
+/// big WHILE to end
+///MENU
+        // Menu
+        Scanner menuSel = new Scanner(System.in);
+        mLoop:
+        while (menuItem != 7) {
+
+            System.out.println("\n\n === MENU === \n");
+            System.out.println("1. Host current Date and Time ");
+            System.out.println("2. Host uptime ");
+            System.out.println("3. Host memory use ");
+            System.out.println("4. Host Netstat ");
+            System.out.println("5. Host current users ");
+            System.out.println("6. Host running processes ");
+            System.out.println("7. Quit \n");
+            System.out.print("\n>>>>>  ");
+
+            menuItem = Character.getNumericValue(menuSel.next().charAt(0));  // char or int?
+            System.out.print("\nSelection:  "+menuItem);
+            if(menuItem == 7) break mLoop;
+        //} // end MENU while
+
+/// 1-7
+        try { /// MAIN TRY
+            InetAddress host = InetAddress.getLocalHost();
+            socket = new Socket(host.getHostName(), mySocket);
+
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            oos.writeObject(menuItem);
+
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            String message;
+            try {  // SUB TRY
+                //while( (message = (String) ois.readObject()) != null) {
+                boolean moreRead = true;
+                while (moreRead) {
+                    message = (String) ois.readObject();
+                    if (message == null) {
+                        moreRead = false;
+                    } else {
+                        System.out.println("\n\n ===== Received from server =====\n " + message +"\n===============================\n\n");
+                    }
+                    ois.close();
+                    oos.close();
+                    socket.close();
+                } // end while
+            } catch (EOFException e) {
+                // end of stream
+                ois.close();
+                oos.close();
+                socket.close();
+            } // END SUB TRY CATCH
+            ois.close();
+            oos.close();
+            socket.close();
+        } catch (UnknownHostException e) {
+            System.out.println("unk host: "+e);
+            //e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("ioe: "+e);
+            //e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("class: "+e);
+            //e.printStackTrace();
+        } /// END MAIN CATCH
+        finally {
+        //ois.close();
+            //oos.close();
+            //socket.close();
+        } // END MAIN TRY CATCH FINALLY
+
+
+        } // end MENU while
+        System.out.println("\n\nEnd of program run.\n");
+    } // end main
+} //end class Requester
---
+++ unf_Server.java
@@ -0,0 +1,165 @@
+
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ */
+public class Server {
+
+    ServerSocket ServerSocket;
+    Socket connection = null;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    String message;
+    int menuItem;
+
+    Server() {
+    }
+
+    void run() {
+        /*
+         Runtime r = Runtime.getRuntime();
+        Process p;     // Process tracks one external native process
+        */
+
+        String cmd = "";
+
+        //cmd = "c:/utils/mydate.bat";
+        //ProcessBuilder pb = new ProcessBuilder(cmd);
+        //  Process p = pb.start();
+        /*
+         Process p = pb.start();
+         InputStream is = p.getInputStream();
+         InputStreamReader isr = new InputStreamReader(is);
+         BufferedReader br = new BufferedReader(isr);
+         String line; */
+        //ObjectOutputStream out;
+        try {
+
+            ServerSocket = new ServerSocket(20904, 10);
+            System.out.println("Waiting for connection");
+
+            connection = ServerSocket.accept();
+            System.out.println("Connected: " + connection.getInetAddress().getHostName());
+
+            in = new ObjectInputStream(connection.getInputStream());
+            menuItem = (int) in.readObject();
+            System.out.println("Read from client: " + menuItem);
+
+            switch (menuItem) {
+                case 1:
+                    System.out.print("\nDate: ");
+                    cmd = "date";
+                    break;
+
+                case 2:
+                    System.out.print("\nuptime: ");
+                    cmd = "uptime";
+                    break;
+                case 3:
+                    System.out.print("\nfree memory: \n");
+                    cmd = "free";
+                    break;
+                case 4:
+                    cmd = "netstat";
+                    break;
+                case 5:
+                    cmd = "who";
+                    break;
+                case 6:
+                    cmd = "ps";
+                    break;
+                case 7:
+                    //  p = r.exec("ls");
+                    break;
+                //  break;
+
+                default:
+                    System.out.println("Invalid selection.");
+                    break;
+            } // end switch menu
+
+            ProcessBuilder pb = new ProcessBuilder(cmd);
+            Process p = pb.start();
+            // process input stream
+            InputStream is = p.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+
+            out = new ObjectOutputStream(connection.getOutputStream());
+            out.flush();
+            ////////////////////////////
+            // run the local command "date"
+            //p = r.exec("date");
+            System.out.println("running local command: " + cmd);
+            ///
+            ///////p = r.exec("c:/utils/mydate.bat");
+            ///
+            //is = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            boolean moreLines = true;
+            //while ((line = br.readLine()) != null) {
+            while (moreLines) {
+                line = br.readLine();
+                if (line != null) {
+                    System.out.println("server>" + line);
+                    //sendMessage(line);
+                    try {
+                        out.writeObject(line);
+                        out.flush();
+                    } catch (IOException ioException) {
+                        //ioException.printStackTrace();
+                    } // end try/catch
+                    System.out.println("Sending: " + line);
+                } else {
+                    System.out.println("\nLINE is NULL\n");
+                    moreLines = false;
+                } // end if/else
+                System.out.println(line);
+            //out = new ObjectOutputStream(connection.getOutputStream());
+                //out.flush();
+
+                p.waitFor();  // wait for process to complete
+          //      System.out.println("Closing Connection....");
+         //       connection.close();  // close this connection
+                //} catch (IOException ioException) {
+            } // end WHILE loop
+
+        } catch (IOException ioException) {
+            /*    System.out.println("Closing Connection: "+ ioException);
+             try {
+             connection.close();  // close this connection
+             } catch (IOException ex) {
+             //Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+             }
+             */
+        } catch (Exception allE) {
+            allE.printStackTrace();
+        }
+        System.out.println("End of RUN");
+    }
+    // } //end class Server
+
+    void sendMessage(String msg) {
+        try {
+            out.writeObject(msg);
+            out.flush();
+            System.out.println("server>" + msg);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+     public static void main(String args[])
+     {
+     Server server = new Server();
+//     while(true){
+     server.run();
+ //    }
+     }
+///////////////}
+} //end class Server
+
